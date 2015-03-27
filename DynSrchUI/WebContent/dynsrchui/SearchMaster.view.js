63bdd186d246f8cda687dbb67e678c060b8e24ae
vaibhavsingh@live.in <vaibhavsingh@live.in>
2015-03-27 11:39:28 -0400

    Initial

---
+++ DynSrchUI/WebContent/dynsrchui/SearchMaster.view.js
@@ -0,0 +1,24 @@
+sap.ui.jsview("dynsrchui.SearchMaster", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf dynsrchui.SearchMaster
+	*/ 
+	getControllerName : function() {
+		return "dynsrchui.SearchMaster";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf dynsrchui.SearchMaster
+	*/ 
+	createContent : function(oController) {
+ 		return new sap.m.Page({
+			title: "Title",
+			content: [
+			
+			]
+		});
+	}
+
+});
